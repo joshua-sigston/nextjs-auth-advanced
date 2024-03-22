8cae10212505d11068faec6b2bc9effdc7cdbbbd
Joshua Sigston <joshua.sigston@outlook.com>
2024-03-22 07:01:31 -0500

    callbacks

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
+import {UserRole} from '@prisma/client'
 import { db } from "./lib/db"
 import {PrismaAdapter} from '@auth/prisma-adapter'
+import { getUserById } from "./helpers/user" 
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +11,31 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      console.log({sessionToken: token, session})
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+    async jwt({token}) {
+      console.log({token})
+      if(!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig
---
+++ next-auth.d.ts
@@ -0,0 +1,22 @@
+import NextAuth, {type DefaultSession} from "next-auth";
+
+export type ExtendedUser = DefaultSession['user']  & {
+  role: UserRole
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser
+  }
+}
+
+import {JWT} from "@auth/core/jwt"
+import { UserRole } from "@prisma/client";
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    role?: "ASMIN" | "USER"
+  }
+}
